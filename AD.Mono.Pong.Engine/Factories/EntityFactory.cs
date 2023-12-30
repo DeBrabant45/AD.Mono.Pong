73f147589cf3d3f3213e0f967c8e1df8789a8fe1
Aaron <DeBrabant45@users.noreply.github.com>
2023-12-30 17:41:48 -0500

    Updating Entity Factories

--- AD.Mono.Pong.Engine/Factories/EntityFactory.cs
+++ AD.Mono.Pong.Engine/Factories/EntityFactory.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework.Content;
 
 namespace AD.Mono.Pong.Engine.Factories;
 
-public abstract class EntityFactory
+public abstract class EntityFactory : IEntityFactory
 {
     public IEntity Create(ContentManager content, GraphicsDeviceManager deviceManager, Vector2 position)
     {
---
+++ AD.Mono.Pong.Engine/Factories/IEntityFactory.cs
@@ -0,0 +1,9 @@
+﻿using AD.Mono.Pong.Engine.Core;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace AD.Mono.Pong.Engine.Factories;
+public interface IEntityFactory
+{
+    public IEntity Create(ContentManager content, GraphicsDeviceManager deviceManager, Vector2 position);
+}
